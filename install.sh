9fbbc5bce4e6deb9e7350b84e17bd933f5b5f924
jbcloud1 <122526383+jbcloud1@users.noreply.github.com>
2023-03-04 11:37:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,31 @@
-
+#!/bin/bash
+echo "Jexactly only work if pterodactyl is installed, do you want to continue"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) make install; break;;
+        No ) exit;;
+    esac
+done
+echo "Updating"
+apt update
+apt upgrade -y
+clear
+echo "Preparing install"
+cp -R /var/www/pterodactyl /var/www/pterodactyl-backup
+mysqldump -u root -p panel > /var/www/pterodactyl-backup/panel.sql
+cd /var/www/pterodactyl
+php artisan down
+echo "Installing"
+curl -L -o panel.tar.gz https://github.com/jexactyl/jexactyl/releases/latest/download/panel.tar.gz
+tar -xzvf panel.tar.gz && rm -f panel.tar.gz
+chmod -R 755 storage/* bootstrap/cache
+composer require asbiin/laravel-webauthn
+composer install --no-dev --optimize-autoloader
+php artisan optimize:clear
+php artisan migrate --seed --force
+chown -R www-data:www-data /var/www/pterodactyl/*
+echo "Finishing install"
+php artisan queue:restart
+php artisan up
+echo "Jexactly have been Installed"
+exit
